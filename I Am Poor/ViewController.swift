fde0b9162a911a9842225a432e3f1011218b8d61
Siddhika Ghaisas <siddhikapg@gmail.com>
2020-05-25 09:11:25 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ketan Jogalekar on 5/25/20.
+//  Copyright © 2020 Siddhika Ghaisas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
